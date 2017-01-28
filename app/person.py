6fdbdc763f51e17e7e3d9199bd5e65dcd1b57312
sirjmkitavi <kitavimjoseph@gmail.com>
2017-01-28 11:49:23 +0300

    Added docstring to models

--- app/person.py
+++ app/person.py
@@ -2,8 +2,8 @@
 
 
 class Person(object):
-    """Person."""
-    def __init__(self, first_name, last_name, role, accomodation='N'):
+    """Person class """
+    def __init__(self, first_name, last_name, role, accomodation="N"):
         self.first_name = first_name
         self.last_name = last_name
         self.role = role
@@ -11,10 +11,12 @@ class Person(object):
 
 
 class Fellow(Person):
+    """ Fellow class. Inheriting class person """
     def __init__(self, first_name, last_name, role, accomodation="N"):
         super(Fellow, self).__init__(first_name, last_name, role, accomodation)
 
 
 class Staff(Person):
+    """ Staff class. Inheriting class person """
     def __init__(self, first_name, last_name, role, accomodation="N"):
-        super(Fellow, self).__init__(first_name, last_name, role, accomodation)
+        super(Staff, self).__init__(first_name, last_name, role, accomodation)
